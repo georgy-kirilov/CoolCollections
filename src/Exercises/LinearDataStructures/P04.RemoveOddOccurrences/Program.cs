2a3a4c1cf31c5b3f8a1b3815a25060b541003431
georgy-kirilov <georgynk17@gmail.com>
2021-03-08 13:47:27 +0200

    Solve problems 4 and 5 from LinearDataStructures exercises

---
+++ src/Exercises/LinearDataStructures/P04.RemoveOddOccurrences/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace P04.RemoveOddOccurrences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var numbersOccurencesPairs = new Dictionary<int, int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (!numbersOccurencesPairs.ContainsKey(numbers[i]))
+                {
+                    numbersOccurencesPairs.Add(numbers[i], 0);
+                }
+                numbersOccurencesPairs[numbers[i]]++;
+            }
+
+            numbersOccurencesPairs = numbersOccurencesPairs.Where(p => p.Value % 2 != 0).ToDictionary(p => p.Key, p => p.Value);
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbersOccurencesPairs.ContainsKey(numbers[i]))
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ src/Exercises/LinearDataStructures/P05.CountOfOccurrences/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace P05.CountOfOccurrences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] occurences = new int[1001];
+            var list = new List<int>();
+
+            foreach (int item in array)
+            {
+                if (!list.Contains(item))
+                {
+                    list.Add(item);
+                }
+                occurences[item]++;
+            }
+
+            list = list.OrderBy(x => x).ToList();
+
+            foreach (int item in list)
+            {
+                Console.WriteLine($"{item} -> {occurences[item]} times");
+            }
+        }
+    }
+}
