1f1f6db0809b07fc996feb8d8be1271c3f2b0762
georgy-kirilov <georgynk17@gmail.com>
2021-03-10 00:34:17 +0200

    Add exercises directory inside the repository

---
+++ exercises/LinearDataStructures/P01.SumAndAverage/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P01.SumAndAverage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+
+            int sum = numbers.Sum();
+            double average = (double)sum / numbers.Count;
+            Console.WriteLine($"Sum={sum}; Average={average:f2}");
+        }
+    }
+}
---
+++ exercises/LinearDataStructures/P02.SortWords/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace P02.SortWords
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<string> words = Console.ReadLine().Split(' ').ToList();
+            words = words.OrderBy(w => w).ToList();
+            string output = string.Join(" ", words);
+            Console.WriteLine(output);
+        }
+    }
+}
